2ca8a6f756cee9be65c0b709d2f5190a469f589f
ASUS <you@example.com>
2026-04-01 22:56:01 -0400

    Add script to retrieve and display Hugging Face space information

---
+++ scripts/get_space_info.py
@@ -0,0 +1,14 @@
+from huggingface_hub import HfApi
+import os, json
+
+repo_id = 'sjhallo07/marcos-mora-chatbot-multimodal'
+token = os.environ.get('HF_TOKEN')
+if not token:
+    print('HF_TOKEN not set')
+    raise SystemExit(2)
+api = HfApi()
+try:
+    info = api.space_info(repo_id, token=token)
+    print(json.dumps(info.__dict__, default=str, indent=2))
+except Exception as e:
+    print('Error:', e)
